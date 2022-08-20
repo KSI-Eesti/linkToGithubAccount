b5ac471f0d795124354cf3370d7f45f75e223595
KSI-Eesti <89138983+KSI-Eesti@users.noreply.github.com>
2022-08-20 23:21:31 +0100

    Link to github account

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('CR7')
+
+  const onClick = ()=> {  
+    Linking.openURL('https://github.com/KSI-Eesti')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>My name is {name}</Text>
+      <Button title='Let us Start' color='#111001' onPress={onClick}></Button>
+      <StatusBar barStyle= 'light-content' />
     </View>
   );
 }
@@ -18,4 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#e11',
+    margin: 20
+  }
 });
